d261c48ca30983effa57294f3a3965321ecb4ce7
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-09-07 02:35:56 +0900

    feat: 기본 자료 세팅

---
+++ src/main/java/com/example/tecotalk/TecotalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.tecotalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecotalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecotalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tecotalk/TecotalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tecotalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecotalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
